44d465f44e378a77af3d5b56f5953d1bae282f01
abdulwasay475o <f2023266932@umt.edu.pk>
2026-02-06 17:28:50 +0000

    Final pipeline trigger

--- app/index.js
+++ app/index.js
@@ -1,5 +1,4 @@
 const express = require('express');
 const app = express();
-app.get('/', (req, res) => res.send('DevSecOps Workshop Working    Successsfully
-!'));
+app.get('/', (req, res) => res.send('DevSecOps Pipeline  Working!'));
 app.listen(3000, () => console.log('Running on port 3000'));
